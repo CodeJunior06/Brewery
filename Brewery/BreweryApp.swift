582589afc8cbb2ba55e3726c91dd32b48e25072b
Santiago Rueda <san1428.srp@gmail.com>
2026-01-14 22:23:37 -0500

    Initial Commit

---
+++ Brewery/BreweryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreweryApp.swift
+//  Brewery
+//
+//  Created by Santiago Rueda on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct BreweryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreweryTests/BreweryTests.swift
@@ -0,0 +1,17 @@
+//
+//  BreweryTests.swift
+//  BreweryTests
+//
+//  Created by Santiago Rueda on 14/01/26.
+//
+
+import Testing
+@testable import Brewery
+
+struct BreweryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
